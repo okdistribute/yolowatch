cd84782743054ff07012d313bbb20f7c1eeb89f6
karissa <krmckelv@gmail.com>
2016-06-20 18:29:29 -0700

    update for streams

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
 var filewatcher = require('filewatcher')
+var from = require('from2')
 var each = require('stream-each')
 var path = require('path')
 var walker = require('folder-walker')
 
-module.exports = function yoloWatch (root, onchange) {
+module.exports = function yoloWatch (root) {
   var stats = {}
-
   var dirs = filewatcher()
-  var watcher = filewatcher()
-  watcher.on('change', fileChanged)
+  dirs.on('change', kick)
   dirs.add(root)
   kick(root)
-  dirs.on('change', function (dir, stat) {
-    kick(dir)
-  })
 
+  var watcher = filewatcher()
+  function read (size, cb) {
+    watcher.on('change', function (filepath, stat) {
+      fileChanged(filepath, stat, cb)
+    })
+  }
   function kick (dir) {
-    var fileStream = walker(dir, {filter: function (data) {
-      return data.indexOf('.dat') === -1
-    }})
-    each(fileStream, function (file, next) {
-      if (!stats[file.filepath]) watcher.add(file.filepath)
-      stats[file.filepath] = file
+    var fileStream = walker(dir)
+    each(fileStream, function (data, next) {
+      if (!stats[data.filepath]) watcher.add(data.filepath)
+      stats[data.filepath] = data
       next()
     }, function (err) {
       if (err) throw err
     })
   }
 
-  function fileChanged (filepath, stat) {
-    var type = 'modified'
-    if (!stat) {
-      type = 'deleted'
-      stats[filepath] = null
-    }
-    else stats[filepath].stat = stat
-    onchange({
+  function fileChanged (filepath, stat, cb) {
+    var item = {
       root: root,
       filepath: filepath,
       stat: stat,
       relname: root === filepath ? path.basename(name) : path.relative(root, filepath),
       basename: path.basename(filepath)
-    })
+    }
+    var isFile = stat.isFile()
+    if (isFile) {
+      item.type = 'file'
+    }
+    var isDir = stat.isDirectory()
+    if (isDir) item.type = 'directory'
+
+    cb(null, item)
   }
+  return from.obj(read)
 }
